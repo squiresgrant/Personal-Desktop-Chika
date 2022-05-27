8fbb965ab9ff874151587cabd92392a7a25f092c
grant-kun <geedub602@gmail.com>
2022-05-27 11:19:45 -0500

    did some stuff, probably last update

--- gif-win32-x64/resources/app/index.ts
+++ gif-win32-x64/resources/app/index.ts
@@ -3,28 +3,45 @@ const { app, BrowserWindow, Notification, globalShortcut   } = require('electron
 
 function showNotification () {
   // @ts-ignore
-  new Notification({ title: 'hewwo;)', body: 'press alt+! to close;)' }).show()
+  new Notification({ title: 'hewwo;)', body: 'press alt+! to evict, press alt+@ to toggle' }).show()
 }
 const createWindow = () => {
     const { screen } = require('electron')
     
     const primaryDisplay = screen.getPrimaryDisplay()
     const { width, height } = primaryDisplay.workAreaSize
+    
 
     const win = new BrowserWindow({
         width: 400,
         height: 400,
         x: width-250,
-  y: height-220,
-        
+        y: height-220,
+        skipTaskbar: true,
         transparent:true,
+        resizable: false,
         frame: false
     })
-  
+
+    
+
     win.loadFile('index.html')
+    var on = true
     win.setIgnoreMouseEvents(true, { forward: true });
-    const ret = globalShortcut.register('Alt+!', () => {
+    globalShortcut.register('Alt+!', () => {
       win.close()
+    })
+    globalShortcut.register('Alt+@', () => {
+      if(on){
+        win.loadFile('') // ! this causes a warning, dont know how to do it better
+      } else {
+        win.loadFile('index.html')
+      }
+      on=!on
+      
+    })
+    win.on('close', () => {
+      globalShortcut.unregisterAll()
     })
     /*
     win.webContents.on("before-input-event", (event, input) => {
@@ -33,11 +50,13 @@ const createWindow = () => {
       }
        
   });*/
-        win.setAlwaysOnTop(true);
+//set window to always on top
+    win.setAlwaysOnTop(true, "float", 1)
     
   }
   app.whenReady().then(() => {
+    showNotification()
     createWindow()
-    showNotification ()
+    
     
   })
--- index.ts
+++ index.ts
@@ -3,13 +3,14 @@ const { app, BrowserWindow, Notification, globalShortcut   } = require('electron
 
 function showNotification () {
   // @ts-ignore
-  new Notification({ title: 'hewwo;)', body: 'press alt+! to close;)' }).show()
+  new Notification({ title: 'hewwo;)', body: 'press alt+! to evict, press alt+@ to toggle' }).show()
 }
 const createWindow = () => {
     const { screen } = require('electron')
     
     const primaryDisplay = screen.getPrimaryDisplay()
     const { width, height } = primaryDisplay.workAreaSize
+    
 
     const win = new BrowserWindow({
         width: 400,
@@ -18,22 +19,30 @@ const createWindow = () => {
         y: height-220,
         skipTaskbar: true,
         transparent:true,
+        resizable: false,
         frame: false
     })
 
-    //never gonna give you up
-    //never gonna let you down
-    //never gonna run around and desert you
-    //never gonna make you cry
-    //never gonna say goodbye
-    //never gonna tell a lie and hurt you
+    
 
     win.loadFile('index.html')
+    var on = true
     win.setIgnoreMouseEvents(true, { forward: true });
     globalShortcut.register('Alt+!', () => {
       win.close()
     })
-    
+    globalShortcut.register('Alt+@', () => {
+      if(on){
+        win.loadFile('') // ! this causes a warning, dont know how to do it better
+      } else {
+        win.loadFile('index.html')
+      }
+      on=!on
+      
+    })
+    win.on('close', () => {
+      globalShortcut.unregisterAll()
+    })
     /*
     win.webContents.on("before-input-event", (event, input) => {
       if(input.key=='+'&&input.alt){
@@ -41,7 +50,8 @@ const createWindow = () => {
       }
        
   });*/
-        win.setAlwaysOnTop(true);
+//set window to always on top
+    win.setAlwaysOnTop(true, "float", 1)
     
   }
   app.whenReady().then(() => {
